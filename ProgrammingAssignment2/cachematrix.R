99ac5e31dadf87bdd5ba6c2e624f170abfecc32a
jeyamooc <jeyamooc@gmail.com>
2015-08-23 02:43:21 +0800

    final submission

---
+++ ProgrammingAssignment2/cachematrix.R
@@ -0,0 +1,32 @@
+## Create matrix and inverse and cache them
+
+## makeCacheMatrix -- Create the matrix and inverse matix in Environmental varialbles
+
+makeCacheMatrix <- function(x = matrix()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) m <<- solve
+  getsolve <- function() m
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
+}
+
+
+## cacheSolve - will cache the inverse matrix
+
+cacheSolve <- function(x, ...) {        
+  m <- x$getsolve()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setsolve(m)
+  m
+}
